4d1b3536b9c1600d32134fb07a24b122a3accb4c
sahiba <25sahiba03@gmail.com>
2026-08-12 20:39:34 +0530

    add post method

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 function App() {
   const [tasks, setTasks] = useState([]);
-  // So for your simple GET request, you don't need to write headers or body.
-  // But fetch() CAN have headers and body  For example, POST:
+
   useEffect(() => {
     fetch("http://localhost:3000/tasks")
       .then((response) => response.json())
@@ -11,9 +10,23 @@ function App() {
         setTasks(data);
       });
   }, []);
+  function newTaskCreated(){
+   
+    fetch("http://localhost:3000/tasks",
+      {
+      method: "POST",
+      headers: {
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify({name:"definitionrevision"})
+  });
+
+  }
   return (
     <div>
       <h1>Todo App</h1>
+      <input type="text" />
+      <button onClick={newTaskCreated}>create</button>
       <h2>Tasks</h2>
       {tasks.map((task) => (
         <p key={task.id}>
