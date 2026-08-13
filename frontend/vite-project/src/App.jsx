4e5b9992a37f7c4ea8c569452437dc77fd9a2611
sahiba <25sahiba03@gmail.com>
2026-08-13 21:30:31 +0530

    add http post method

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [task,setTask]= useState("");
 
   useEffect(() => {
     fetch("http://localhost:3000/tasks")
@@ -10,27 +11,31 @@ function App() {
         setTasks(data);
       });
   }, []);
-  function newTaskCreated(){
-   
-    fetch("http://localhost:3000/tasks",
-      {
-      method: "POST",
-      headers: {
-        "Content-Type":"application/json"
-      },
-      body: JSON.stringify({name:"definitionrevision"})
-  });
-
-  }
+  
+function newTaskCreated(){
+  fetch("http://localhost:3000/tasks",{
+    method:"POST",
+    headers:{
+      "Content-Type":"application/json",
+    },
+    body:JSON.stringify({name:task})
+  })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setTasks([...tasks,data])
+        setTask("");
+      });
+}
   return (
     <div>
       <h1>Todo App</h1>
-      <input type="text" />
-      <button onClick={newTaskCreated}>create</button>
+     <input type="text" value={task} onChange={(e)=>{setTask(e.target.value)}}/>
+     <button onClick={newTaskCreated}>create</button>
       <h2>Tasks</h2>
       {tasks.map((task) => (
-        <p key={task.id}>
-          {task.id} - {task.name}
+        <p >
+         {task.name}
         </p>
       ))}
     </div>
