ce7d82b23a203dbe588a9443176682e1a841cc3c
sahiba <25sahiba03@gmail.com>
2026-08-13 21:32:27 +0530

    add post method

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 const tasks = [
   {
@@ -30,7 +31,6 @@ app.post("/tasks", (req, res) => {
  };
  tasks.push(task);
  res.json(task);
-
 });
 
 app.listen(3000, () => {
