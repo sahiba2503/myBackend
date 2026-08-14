87e0b893894b361734f7b6504fc467fc60fc21bf
sahiba <25sahiba03@gmail.com>
2026-08-14 20:48:47 +0530

    add http delete method

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,44 +1,88 @@
+
+
 import { useEffect, useState } from "react";
+
 function App() {
   const [tasks, setTasks] = useState([]);
-  const [task,setTask]= useState("");
+  const [task, setTask] = useState("");
 
+  // GET method
   useEffect(() => {
-    fetch("http://localhost:3000/tasks")
+    fetch("http://localhost:3000/tasks", {
+      method: "GET",
+    })
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
         setTasks(data);
       });
   }, []);
-  
-function newTaskCreated(){
-  fetch("http://localhost:3000/tasks",{
-    method:"POST",
-    headers:{
-      "Content-Type":"application/json",
-    },
-    body:JSON.stringify({name:task})
-  })
+
+  // POST method
+  function newTaskCreated() {
+    fetch("http://localhost:3000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: task,
+      }),
+    })
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        setTasks([...tasks,data])
+
+        setTasks([...tasks, data]);
+
         setTask("");
       });
-}
+  }
+
+  // DELETE method
+  function deleteTask(id) {
+    fetch("http://localhost:3000/tasks", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: id,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+
+        setTasks(tasks.filter((task) => task.id !== id));
+      });
+  }
+
   return (
     <div>
       <h1>Todo App</h1>
-     <input type="text" value={task} onChange={(e)=>{setTask(e.target.value)}}/>
-     <button onClick={newTaskCreated}>create</button>
+
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+
+      <button onClick={newTaskCreated}>Create</button>
+
       <h2>Tasks</h2>
+
       {tasks.map((task) => (
-        <p >
-         {task.name}
-        </p>
+        <div key={task.id} className="taskcontainer">
+          <p>{task.name}</p>
+
+          <button onClick={() => deleteTask(task.id)}>
+            Delete
+          </button>
+        </div>
       ))}
     </div>
   );
 }
+
 export default App;
